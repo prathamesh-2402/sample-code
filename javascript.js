bf40afb80f15281bfc6e4e5a45729f06f7f8d592
sarwaShaktishali <badassgamer2407@gmail.com>
2021-10-21 21:36:30 +0530

    created a random gradient generator

--- javascript.js
+++ javascript.js
@@ -34,3 +34,27 @@ document.getElementsByClassName("icon")[0].addEventListener('click',()=>{
     console.log(unshow)
  
 })
+
+function generate(){
+    
+    const hex = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
+    let i = 0;
+    function hexGenerate(a) {
+        while(i<6) {
+          a += hex[Math.round( Math.random() * 15 )];
+          i++;
+        }
+        i = 0;
+        return a;
+    }
+
+    // let newColor1 = hexGenerate('#');
+    // let newColor2 = hexGenerate('#');
+    let angle = Math.round( Math.random() * 360 );
+
+    let gradient = `linear-gradient(${angle}deg, ${hexGenerate('#')}, ${hexGenerate('#')});`
+
+    document.querySelector('body').style = `background: ${gradient}`;
+    document.querySelector('.output').textContent = `background: ${gradient}`;
+    
+}
